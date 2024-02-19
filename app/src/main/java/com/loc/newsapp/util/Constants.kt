97951aa5c5846013cc98b6063fbf6d7341ca18fb
Praveen Sundriyal <ranveerwebs@gmail.com>
2024-02-19 20:36:50 +0530

    News App Jetpack Compose

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.loc.newsapp.util
+
+object Constants {
+    const val USER_SETTINGS = "userSettings"
+    const val APP_ENTRY = "appEntry"
+    const val API_KEY = "1132e63a0c0642c9878df798c8ebf2a2"
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val NEWS_DATABASE_NAME = "news_db"
+
+}
